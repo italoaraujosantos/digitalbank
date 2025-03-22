fae28459d1ccae45ca4b92bb472754727f01f9b7
Italo Araujo Chaves <italoaraujosantos@gmail.com>
2025-03-22 00:06:09 -0300

    Create project

---
+++ src/main/java/br/com/simple_transaction/DigitalbankApplication.java
@@ -0,0 +1,13 @@
+package br.com.simple_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankApplication.class, args);
+	}
+
+}
